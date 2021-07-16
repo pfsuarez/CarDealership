2844e37efa50139cf8adbaa0e93335928d6945a6
Pedro Suarez <pfsuarez@gmail.com>
2021-07-16 15:44:09 +0200

    Added cors functionality

--- src/cardealership.api/Startup.cs
+++ src/cardealership.api/Startup.cs
@@ -17,53 +17,68 @@ using Microsoft.OpenApi.Models;
 
 namespace cardealership.api
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<CarDealershipDbContext>(options => {
-                options.UseInMemoryDatabase("CarDealerShipDB");
-            });
-
-            services.AddScoped<IVehicleRepository, VehicleRepository>();
-
-            services.AddControllers(options => {
-                options.SuppressAsyncSuffixInActionNames = false;
-            });
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "cardealership.api", Version = "v1" });
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "cardealership.api v1"));
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<CarDealershipDbContext>(options =>
+      {
+        options.UseInMemoryDatabase("CarDealerShipDB");
+      });
+
+      services.AddScoped<IVehicleRepository, VehicleRepository>();
+
+      services.AddControllers(options =>
+      {
+        options.SuppressAsyncSuffixInActionNames = false;
+      });
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "cardealership.api", Version = "v1" });
+      });
+      services.AddCors(options =>
+      {
+        options.AddPolicy("CorsEnabledSites",
+              builder =>
+              {
+            builder.AllowAnyMethod()
+                          .AllowAnyHeader()
+                          .AllowAnyOrigin();
+
+          });
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "cardealership.api v1"));
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseCors("CorsEnabledSites");
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
