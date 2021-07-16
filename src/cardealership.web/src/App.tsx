d065cf4c1dd6d5b876bbbd98a2bfda8bde6fc333
Pedro Suarez <pfsuarez@gmail.com>
2021-07-16 19:35:00 +0200

    Moved load vechicles from app to carlist component

--- src/cardealership.web/src/App.tsx
+++ src/cardealership.web/src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useCallback, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import "./App.css";
 
@@ -7,19 +6,7 @@ import CarList from "./Containers/CarList/CarList";
 import CarView from "./Containers/CarView/CarView";
 import Layout from "./Hoc/Layout/Layout";
 
-import * as actions from "./Store/Actions/App";
-
 function App() {
-  const dispatch = useDispatch();
-  const onStartup = useCallback(
-    () => dispatch(actions.FetchCars()),
-    [dispatch]
-  );
-
-  useEffect(() => {
-    onStartup();
-  }, [onStartup]);
-
   return (
     <Layout>
       <Route path="/" exact component={CarList} />
--- src/cardealership.web/src/Containers/CarList/CarList.tsx
+++ src/cardealership.web/src/Containers/CarList/CarList.tsx
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
 
 import Table from "../../Components/Table/Table";
 import Vehicle from "../../Model/Vehicle";
 import { appState } from "../../Store/Reducers/App";
+import * as actions from "../../Store/Actions/App";
 
 interface Props {}
 
 const CarList: React.FC = () => {
+  const history = useHistory();
   const currentState: appState = useSelector<appState, appState>((state) => {
     return state;
   });
-
   const dispatch = useDispatch();
+  const onLoadVehicles = useCallback(
+    () => dispatch(actions.FetchVehicles()),
+    [dispatch]
+  );
+
+  useEffect(() => {
+    onLoadVehicles();
+  }, [onLoadVehicles]);
+
+  const onNavigateToAddNewVehicleHandler = () => {
+    history.push('/view');
+  };
 
   const vehicleSchema: Vehicle = {
     id: "000",
@@ -40,6 +53,12 @@ const CarList: React.FC = () => {
 
       <br />
       <NavLink to="/view">Add Vehicle</NavLink>
+      <input
+        className="btn btn-primary"
+        type="button"
+        value="Add Vehicle"
+        onClick={onNavigateToAddNewVehicleHandler}
+      />
     </>
   );
 };
